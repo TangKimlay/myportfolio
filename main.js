3c4709dec96fbcb31091d8880454c4c844f90e9d
kimlay <tangkimlay13@gmail.com>
2026-05-18 12:04:12 +0700

    edit code

--- main.js
+++ main.js
@@ -62,9 +62,9 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal(".home-content, heading", {origin: "top"});
-ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, contact", {origin: "buttom"});
+ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact", {origin: "buttom"});
 ScrollReveal().reveal(".home-content h1, .aboutme-img, .education-img", {origin: "left"});
-ScrollReveal().reveal(".home-contat p, .aboutme-content, education-content", {origin: "right"});
+ScrollReveal().reveal(".home-content p, .aboutme-content, .education-content", {origin: "right"});
 
 // typed js
 const typed = new Typed(".multiple-text", {
