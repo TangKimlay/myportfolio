801b0eb189bf6f1c879ca539f70d0ca5071cec13
kimlay <tangkimlay13@gmail.com>
2026-05-19 09:36:49 +0700

    update code

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal(".home-content, .heading", {origin: "top"});
-ScrollReveal().reveal(".home-img, .services-container, .portfolio-box, .contact", {origin: "buttom"});
+ScrollReveal().reveal(".home-img, .services-container, .project-box, .contact", {origin: "buttom"});
 ScrollReveal().reveal(".home-content h1, .aboutme-img, .education-img", {origin: "left"});
 ScrollReveal().reveal(".home-content p, .aboutme-content, .education-content", {origin: "right"});
 
